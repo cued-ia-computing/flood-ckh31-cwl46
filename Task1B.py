eadb1df988d5fa55421d12c6b8a8b4ae5bf0afe8
Cameron Howcutt <ckh31@cam.ac.uk>
2021-01-27 16:15:07 +0000

    Completed Task B

--- Task1B.py
+++ Task1B.py
@@ -2,7 +2,7 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
 
-def runB():
+def run():
 
     stations = build_station_list
     p = (52.2053, 0.1218)
@@ -17,6 +17,6 @@ def runB():
     else:
         print(List[:-10])
 
-
-print("*** Task 1B: CUED Part IA Flood Warning System ***")
-runB()
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
