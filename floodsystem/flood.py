0c7a752433a4d1b1b399bbc66a57f091af0dd074
Cameron Howcutt <ckh31@cam.ac.uk>
2021-02-25 16:19:57 +0000

    Completed Task2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,7 +6,6 @@ from .stationdata import build_station_list
 
 def stations_level_over_threshold(stations, tol):
 
-    stations = build_station_list()
     F = []
     r = [station.relative_water_level() for station in stations]
     i = 0
@@ -19,25 +18,21 @@ def stations_level_over_threshold(stations, tol):
         else:
             i += 1
     
-    F = sorted_by_key(F, 1)
+    F = sorted_by_key(F, 1, True)
     return F
 
 
 def stations_highest_rel_level(stations, N):
 
-    stations = build_station_list()
     r = [station.relative_water_level() for station in stations]
-    i = 0
     S = []
     L = []
+    i = 0
 
     for station in stations:
         q = (station.name, r[i])
         S.append(q)
 
-    S = sorted_by_key(S,1)
+    S = sorted_by_key(S, 1, True)
 
-    for i in range(N):
-        L.append(S[i])
-        i +=1
-    return L
+    return S[:N]
